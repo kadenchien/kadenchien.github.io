f9cf0be025a539cc28b51161a16e9fca35667dc3
Kaden Chien <kaden.chien@gmail.com>
2024-07-03 01:40:37 -0400

    added interests section

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,6 +250,43 @@
       clickable: true
     }
   });
+  
+    /**
+   * Init swiper sliders
+   */
+    function initSwiper() {
+      document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+        let config = JSON.parse(
+          swiperElement.querySelector(".swiper-config").innerHTML.trim()
+        );
+  
+        if (swiperElement.classList.contains("swiper-tab")) {
+          initSwiperWithCustomPagination(swiperElement, config);
+        } else {
+          new Swiper(swiperElement, config);
+        }
+      });
+    }
+  
+    window.addEventListener("load", initSwiper);
+  
+    /**
+     * Correct scrolling position upon page load for URLs containing hash links.
+     */
+    window.addEventListener('load', function(e) {
+      if (window.location.hash) {
+        if (document.querySelector(window.location.hash)) {
+          setTimeout(() => {
+            let section = document.querySelector(window.location.hash);
+            let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+            window.scrollTo({
+              top: section.offsetTop - parseInt(scrollMarginTop),
+              behavior: 'smooth'
+            });
+          }, 100);
+        }
+      }
+    });
 
   /**
    * Animation on scroll
